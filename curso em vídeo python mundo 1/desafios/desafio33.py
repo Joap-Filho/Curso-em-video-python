09b06413ffb39c6e9a0bfd3e64bba821104b1004
João Cruz de Farias Filho <fjoao.developer@gmail.com>
2023-05-22 22:00:59 -0300

    Update desafio33.py

--- curso em vídeo python mundo 1/desafios/desafio33.py
+++ curso em vídeo python mundo 1/desafios/desafio33.py
@@ -5,5 +5,9 @@ n3 = int(input('Informe outro número: '))
 lista = [n1 , n2, n3]
 maior = max(lista)
 menor = min(lista)
+ordem_crescente = sorted(lista)
+ordem_decrescente = sorted(lista, reverse=True)
 
 print(f'O menor valor digitado é {menor} e o maior é {maior}') 
+print(f'Os números na ordem crescente são {ordem_crescente}')
+print(f'A ordem descrente é {ordem_decrescente}')
