b40c49b45c6f21ce1b5f1cf1d0f58e8b49d98838
João Cruz de Farias Filho <fjoao.developer@gmail.com>
2023-05-23 00:05:39 -0300

    add cores

--- curso em vídeo python mundo 1/desafios/desafio34.py
+++ curso em vídeo python mundo 1/desafios/desafio34.py
@@ -1,9 +1,11 @@
 salario = float(input('Informe o seu salário: R$'))
+cores = {'fim':'\033[m','amarelo':'\033[0;33m','ciano':'\033[0;34m'}
+
 
 if salario > 1250:
     aumento = salario * 0.10
-    print(f'Seu salário foi aumentado em 10% ({aumento} e ficou no valor de {salario + aumento}')
+    print(f'Seu salário foi aumentado em 10% ({cores["amarelo"]}R${aumento:.2f}{cores["fim"]}e ficou no valor de {cores["ciano"]}R${aumento + salario:.2f}{cores["fim"]}')
 
 if salario <= 1250:
     aumento = salario * 0.15
-    print(f'Seu salário foi aumentado em 15% (R${aumento:.2f}) e ficou no valor de R${salario + aumento:.2f})')
+    print(f'Seu salário foi aumentado em 15% ({cores["amarelo"]}R${aumento:.2f}{cores["fim"]} e ficou no valor de {cores["ciano"]}R${salario + aumento:.2f}{cores["fim"]}')
--- curso em vídeo python mundo 1/desafios/desafio35.py
+++ curso em vídeo python mundo 1/desafios/desafio35.py
@@ -2,7 +2,14 @@ r1 = float(input('Informe o valor da primeira reta: '))
 r2 = float(input('Informe o valor da segunda reta: '))
 r3 = float(input('Informe o valor da terceira reta: '))
 
-if r2 + r3 > r1 and r1 + r3 > r2 and r1 + r2 > r3:
-    print('Pode formar um triêngulo')
-else:
+if r1 < r2 + r3 and r2 < r3 + r1 and r3 < r1 + r2:
+    print('Pode formar um triângulo')
+    if r1 == r2 == r3:
+        print('E é Equilátero')
+    if r1 != r2 != r3 and r1 != r3:
+        print('E é escaleno')
+    if (r1 == r2 != r3) or (r2 == r3 != r1) or (r1 == r3 != r2):
+        print('E é isósceles')
+    
+else: 
     print('Não pode formar um triângulo')
