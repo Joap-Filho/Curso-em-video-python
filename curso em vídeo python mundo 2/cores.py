b83e1b88ffe20c20b72047010e0b9676af7b7182
João Cruz de Farias Filho <fjoao.developer@gmail.com>
2023-05-30 10:47:23 -0300

    mundo-2

---
+++ curso em vídeo python mundo 2/cores.py
@@ -0,0 +1,25 @@
+negrito = '\033[1m' + 'negrito' + '\033[m'
+sublinhado = '\033[4m' + 'sublinhado' + '\033[m'
+
+
+vermelho = '\033[0;31m' + 'vermelho' + '\033[m'
+verde = '\033[0;32m' + 'verde' + '\033[m'
+amarelo = '\033[0;33m' + 'amarelo' + '\033[m'
+azul = '\033[0;34m' + 'azul' + '\033[m'
+roxo = '\033[0;35m' + 'roxo' + '\033[m'
+ciano = '\033[0;36m' + 'verde' + '\033[m'
+cinza = '\033[0;37m' + 'cinza' + '\033[m'
+
+print('')
+print(negrito)
+print(sublinhado)
+print('')
+
+print(vermelho)
+print(verde)
+print(amarelo)
+print(azul)
+print(roxo)
+print(ciano)
+print(cinza)
+
