6ba1aeeb2ee090b376d4f63d57ed167e9889fc75
João Cruz de Farias Filho <fjoao.developer@gmail.com>
2023-05-30 12:17:17 -0300

    mais desafios

---
+++ desafios_mundo2.py/desafio38.py
@@ -0,0 +1,9 @@
+n1 = int(input('Informe o primeiro número: '))
+n2 = int(input('Informe o segundo número: '))
+
+if n1 > n2:
+    print(f'O primeiro número {n1} é maior que o segundo número {n2}')
+if n2 > n1:
+    print(f'O segundo número {n2} é maior que o prmeiro núero {n1}') 
+if n1 == n2:
+    print('Não existe número maior, os dois são iguais. ')
